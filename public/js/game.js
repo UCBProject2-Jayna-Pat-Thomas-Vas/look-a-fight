35273eedaecf763f500f1bb047e73bcabc56b349
thomas cote <tcote19@gmail.com>
2018-03-05 19:40:26 -0800

    updated scripts

--- public/js/game.js
+++ public/js/game.js
@@ -1,5 +1,13 @@
+// Getting user's data at sign-up
+$("#sign-up-submit").on("click", function(){
+	var name = $("#signup-name").val();
+	var username = $("#signup-username").val();
+	var email = $("#signup-email").val();
+	var password = $("#signup-password").val();
+});
 
 // Pull in random celebrity to battle with.
+
 
 // Pull user's celebrity data into battle room with random opponent celebrity.
 
