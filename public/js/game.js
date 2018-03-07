4555125db896adbce930a4fc1642c13b0e4583e7
Jayna Neilson <jaynaneilson@Jaynas-MacBook-Pro.local>
2018-03-06 18:17:47 -0800

    changed audio buttons to player

--- public/js/game.js
+++ public/js/game.js
@@ -9,16 +9,16 @@ $("#sign-up-submit").on("click", function(){
 
 // Play and pause music 
 
-var audioElement = document.createElement("audio");
-  audioElement.setAttribute("src", "audio/battle-begins.mp3");
+// var audioElement = document.createElement("audio");
+//   audioElement.setAttribute("src", "audio/battle-begins.mp3");
 
-       $(".play-button").on("click", function() {
-         audioElement.play();
-       });
+//        $(".play-button").on("click", function() {
+//          audioElement.play();
+//        });
 
-       $(".pause-button").on("click", function() {
-         audioElement.pause();
-       });
+//        $(".pause-button").on("click", function() {
+//          audioElement.pause();
+//        });
 
 
 // Pull in random celebrity to battle with.
